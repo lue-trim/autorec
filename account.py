d859e9bf7856b686117fe85417bf2a69473e42a0
luetrim <leavesleviathan@outlook.com>
2025-05-17 10:43:39 +0800

    强制增加bili_ticket和buvid

--- account.py
+++ account.py
@@ -1,10 +1,11 @@
 import autorec
-import os, sys, getopt, time, re, requests, json
+import os, sys, getopt, time, traceback, json
 
 import bilibili_api as bili
 #import bilibili_api.login
 from bilibili_api import login_v2, request_settings
 from bilibili_api.login_v2 import QrCodeLoginEvents
+from bilibili_api.utils.network import get_buvid, get_bili_ticket
 from static import logger, config
 
 def usage():
@@ -39,28 +40,28 @@ def dump_credential(credential:bili.Credential):
     with open("credential.json", 'w') as f:
         json.dump(credential_dict, f)
 
-def login(is_tv=False):
+async def login(is_tv=False):
     '登录账号'
     if is_tv:
         qr = login_v2.QrCodeLogin(platform=login_v2.QrCodeLoginChannel.TV)
     else:
         qr = login_v2.QrCodeLogin(platform=login_v2.QrCodeLoginChannel.WEB) # 生成二维码登录实例，平台选择网页端
-    bili.sync(qr.generate_qrcode())                                          # 生成二维码
+    await qr.generate_qrcode()                                          # 生成二维码
     while not qr.has_done():
         print(qr.get_qrcode_terminal())                                     # 生成终端二维码文本，打印
-        while bili.sync(qr.check_state()) != QrCodeLoginEvents.TIMEOUT:                                            # 在完成扫描前轮询
+        while (await qr.check_state()) != QrCodeLoginEvents.TIMEOUT:                                            # 在完成扫描前轮询
             # print(bili.sync(qr.check_state()))                                   # 检查状态
             time.sleep(2)                                                   # 轮询间隔建议 >=1s
     credential = qr.get_credential()
     #credential = bili.login.login_with_qrcode_term()
-    if not bili.sync(credential.check_valid()):
+    if not (await credential.check_valid()):
         ans = input("\nWarning: this account maybe invalid, continue?(y/N)")
         if ans.lower() != 'y':
             return
     print("Login complete, syncing to blrec...")
 
     # 保存并同步
-    sync_cookies(credential=credential)
+    await sync_cookies(credential=credential)
 
 async def refresh_cookies(is_forced=False):
     '刷新cookies'
@@ -81,28 +82,47 @@ async def refresh_cookies(is_forced=False):
     await credential.refresh()
 
     # 保存并同步
-    sync_cookies(credential=credential)
+    await sync_cookies(credential=credential)
 
-def sync_cookies(credential=None):
+async def sync_cookies(credential:bili.Credential=None):
     '保存cookies并同步到blrec'
     if not credential:
         credential = load_credential()
     else:
+        if not credential.has_buvid3() or not credential.has_buvid4():
+            credential.buvid3, credential.buvid4 = await get_buvid()
         dump_credential(credential)
-    new_cookies = cookie_dict2str(credential.get_cookies())[:-1] # 去除最后的分号
+    credential_dict = credential.get_cookies()
+    bili_tickets = await try_bili_ticket(credential)
+    if bili_tickets:
+        credential_dict.update(bili_tickets)
+    new_cookies = cookie_dict2str(credential_dict)[:-1] # 去除最后的分号
 
     # 更新blrec的cookies
     new_data = {"header": {"cookie": new_cookies}}
     session = autorec.AutoRecSession(config.app['max_retries'])
-    bili.sync(session.set_blrec(new_data))
+    await session.set_blrec(new_data)
 
     print(new_cookies)
     print("Cookies sync complete.")
 
+async def try_bili_ticket(credential:bili.Credential):
+    '尝试获取bili_ticket'
+    try:
+        bili_ticket, bili_ticket_expires = await get_bili_ticket(credential)
+    except Exception:
+        logger.warning(traceback.format_exc())
+        return {}
+    else:
+        return {
+            'bili_ticket': bili_ticket, 
+            'bili_ticket_expires': bili_ticket_expires
+            }
+
 def main():
     # 初始化
     #request_settings.set_enable_bili_ticket(True)
-    request_settings.set_enable_auto_buvid(True)
+    # request_settings.set_enable_auto_buvid(True)
     is_forced = False
     is_refresh_cookies = False
     is_login = False
@@ -126,11 +146,11 @@ def main():
             is_tv = True
 
     if is_login:
-        login(is_tv)
+        bili.sync(login(is_tv))
     if is_refresh_cookies:
         bili.sync(refresh_cookies(is_forced))
     if is_sync:
-        sync_cookies()
+        bili.sync(sync_cookies())
 
 if __name__ == "__main__":
     main()
